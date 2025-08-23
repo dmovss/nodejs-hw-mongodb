fc069ed4f53c1a144cef597120f833d4b457c815
dmovss <d.vovk2018@gmail.com>
2025-08-23 17:30:26 +0100

    fix3

--- src/middlewares/errorHandler.js
+++ src/middlewares/errorHandler.js
@@ -1,12 +1,27 @@
-const errorHandler = (err, req, res, next) => {
-  const status = err.status || 500;
-  const message = err.message || 'Internal Server Error';
+import createHttpError from 'http-errors';
 
-  res.status(status).json({
-    status: 'error',
-    code: status,
-    message,
-  });
+const errorHandler = (error, req, res, next) => {
+  let statusCode = 500;
+  let errorMessage = 'Internal Server Error';
+
+  if (error instanceof mongoose.Error.ValidationError) {
+    statusCode = 400;
+    errorMessage = error.message;
+  } else if (error instanceof mongoose.Error.CastError) {
+    statusCode = 400;
+    errorMessage = 'Invalid ID';
+  } else if (error instanceof createHttpError.HttpError) {
+    statusCode = error.status;
+    errorMessage = error.message;
+  }
+
+  if (error.code === 11000) {
+    statusCode = 409;
+    errorMessage = 'Email already exists';
+  }
+
+  console.error('Error:', error);
+  res.status(statusCode).json({ error: errorMessage });
 };
 
-export { errorHandler };
+export default errorHandler;
