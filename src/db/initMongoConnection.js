39425c73826ea5a2fa9cba17f91f57766ffa21df
dmovss <d.vovk2018@gmail.com>
2025-09-13 21:12:23 +0100

    h1

--- src/db/initMongoConnection.js
+++ src/db/initMongoConnection.js
@@ -1,11 +1,20 @@
 import mongoose from 'mongoose';
 
+import { getEnvVar } from '../utils/getEnvVar.js';
+
 export const initMongoConnection = async () => {
   try {
-    await mongoose.connect(process.env.MONGODB_URI);
-    console.log('Database connection successful');
-  } catch (error) {
-    console.error('Error connecting to database:', error);
-    process.exit(1);
+    const user = getEnvVar('MONGODB_USER');
+    const pwd = getEnvVar('MONGODB_PASSWORD');
+    const url = getEnvVar('MONGODB_URL');
+    const db = getEnvVar('MONGODB_DB');
+
+    await mongoose.connect(
+      `mongodb+srv://${user}:${pwd}@${url}/${db}?retryWrites=true&w=majority`,
+    );
+    console.log('Mongo connection successfully established!');
+  } catch (e) {
+    console.log('Error while setting up mongo connection', e);
+    throw e;
   }
 };
